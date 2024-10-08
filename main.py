12c621a1c64bc32e880a5c1c1477413339c815ae
xSzymonKietlinskix <szymon.kietlinski@gmail.com>
2024-10-08 17:04:36 +0200

    Added ex1 function, generating random numbers. Added xlsx file /w histrograms for distributions. To do: export data from circle area and finish xlsx.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import numpy as np
+from numpy.random import normal
+
 
 def normal_distribution(mean, std_dev, size):
     return np.random.normal(mean, std_dev, size)
@@ -6,29 +8,42 @@ def normal_distribution(mean, std_dev, size):
 def uniform_distribution(low, high, size):
     return np.random.uniform(low, high, size)
 
-def show_circle_shots(amount_of_shots, radius):
-    x = uniform_distribution(-radius, radius, amount_of_shots)
-    y = uniform_distribution(-radius, radius, amount_of_shots)
+def show_circle_shots(amount_of_shots, radius, x):
+    # losowanie współrzędnych punktów
+    x = uniform_distribution(-x, x, amount_of_shots)
+    y = uniform_distribution(-x, x, amount_of_shots)
     shots = np.array([x, y]).T
 
     inside_circle = []
     counter = 0
     for shot in shots:
+        counter += 1
         if np.linalg.norm(shot) <= radius:
             inside_circle.append(shot)
-        if counter % 1000 == 0 and counter != 0:
-            print(f"Shot {len(inside_circle)} : {counter}")
-            print(f"Pole kola: {np.pi * radius**2}")
-            print(f"Procent trafien: {4 * radius * radius * len(inside_circle) / counter}\n\n")
-        counter += 1
+        if counter == 100 or counter == 1000:
+            print(f"Proporcja trafień do strzałów: {len(inside_circle)} : {counter}\n")
+            print(f"Pole koła liczone z proporcji:: {4 * x**2 * len(inside_circle) / counter}\n\n")
+            print(f"Rzeczywiste pole kola: {np.pi * radius**2}")
 
-    print(f"Amount of shots inside the circle: {len(inside_circle)} / {len(shots)}")
+    print(f"Proporcja trafień do strzałów: {len(inside_circle)} : {counter}\n")
+    print(f"Pole koła liczone z proporcji:: {4 * x ** 2 * len(inside_circle) / counter}\n\n")
+    print(f"Rzeczywiste pole kola: {np.pi * radius ** 2}")
 
-    return shots
 
+
+def exercise_1():
+    # rozkład równomierny
+    uniform_dist_numbers = uniform_distribution(-100, 100, 1000)
+    print(f"Wygenerowne liczby z rozkładem równomiernym: {uniform_dist_numbers}\n\n")
+    np.savetxt("uniform_dist_numbers.txt", uniform_dist_numbers, fmt=".6f")
+
+    # rozkład normalny
+    normal_dist_numbers = normal_distribution(0,50,1000)
+    print(f"Wygenerowne liczby z rozkładem normalnym: {normal_dist_numbers}\n\n")
+    np.savetxt("normal_dist_numbers.txt", normal_dist_numbers)
 
 def main():
-    show_circle_shots(10000, 2)
+    exercise_1()
 
 if __name__ == '__main__':
     main()
